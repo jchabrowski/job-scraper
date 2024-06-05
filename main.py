f36ece74748ce71f972083f9a42c014859fe5fad
jchabrowski <chabrowski.kuba@gmail.com>
2024-06-05 16:17:32 +0200

    fix company name xpath, add colors, create 2 separate for loops

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import os
 import time
+from termcolor import cprint
 
 # XPATHS ARE STORED IN .ENV, SO THE JOB PORTALS I SCRAPE ARE NOT THAT OBVIOUS
 load_dotenv()
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
+
+print_red_on_cyan = lambda x: cprint(x, "red", "on_cyan")
+print_red_on_yellow = lambda x: cprint(x, "red", "on_yellow")
+print_black_on_red = lambda x: cprint(x, "black", "on_red")
 
 # navigate to specified url, most work platforms keep data in url params.
 driver.get(os.getenv("BASE_URL"))
@@ -63,21 +68,18 @@ def main_loop(i):
         )
 
     my_keywords = ['fullstack', 'full stack', 'frontend', 'react', 'javascript', 'js', 'node', 'next', 'next.js']
+    restricted_keywords = ['junior', 'qa', 'tester', 'java', 'lead', 'leader', 'staff']
 
     for index, child in enumerate(child_elements):
         WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.XPATH, os.getenv("OFFERS_CONTAINER_XPATH")))
         )
         
-        # print('OFFERS_CONTAINER_XPATH located')
-
         offers_element = driver.find_element(By.XPATH, os.getenv("OFFERS_CONTAINER_XPATH"))
 
         WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.XPATH, os.getenv("OFFER_TITLE_ELEMENT")))
         )
-
-        # print('OFFER_TITLE_ELEMENT located')
 
         child_elements =  offers_element.find_elements(By.XPATH, "./*")
 
@@ -96,11 +98,12 @@ def main_loop(i):
             salary = child.find_element(By.XPATH, os.getenv("SINGLE_OFFER_SALARY")).text
 
         except NoSuchElementException:
-            print(f'Child element {index} with title <<<{title}>>> is missing salary.')
+            print_red_on_yellow(f'Child element {index} with title <<<{title}>>> is missing salary.')
             continue
 
         try:
-            company_name = child.find_element(By.XPATH, os.getenv("SINGLE_OFFER_COMPANY_NAME")).text
+            company_name_elements = child.find_elements(By.XPATH, os.getenv("SINGLE_OFFER_COMPANY_NAME"))
+            company_name = company_name_elements[1].text
 
         except NoSuchElementException:
             print(f'Child element {index} with title <<<{title}>>> is missing company name.')
@@ -108,20 +111,18 @@ def main_loop(i):
             # anchor_tag_value = driver.find_element(By.XPATH, './/a[@data-test="link-offer"]').get_attribute('href')
 
         try:
-
-            for keyword in my_keywords:
+            for keyword in restricted_keywords:
                 if keyword in title.lower():
-                    print(f'Found <<<{title}>>> <<<{salary}>>> at <<{company_name}>>')
-                    # driver.implicitly_wait(1) 
+                    print_black_on_red(f'RESTRICTED KEYWORD ----- {keyword} >>> in <<<{title}>>> <<<{salary}>>> at <<{company_name}>>')
 
-                    # matched_offers.add({title, salary, company_name})
+            for keyword2 in my_keywords:
+                if keyword2 in title.lower():
+                    print_red_on_cyan(f'Found <<<{title}>>> <<<{salary}>>> at <<{company_name}>>')
+        
+            # matched_offers.add({title, salary, company_name})
 
         except Exception as e:
             print(f'ERROR PROCESSING CHILD ELEMENT: {e}')
-
-    # href_value = anchor_tag.get_attribute('href')
-
-# print(f"Scrapping: {len(children_elements)} offers" )
 
 WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, os.getenv("MAX_PAGINATION")))
